f9d193d522f0e705639eeec9e6f4ee49eda5abca
Ryan Kaplan <ryan_kaplan@me.com>
2017-10-09 13:31:15 -0700

    Adds feature for user to create a new note.

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const { MongoClient } = require('mongodb')
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+
+MongoClient.connect('mongodb://localhost/notebook', (err, db) => {
+
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+
+  const notes = db.collection('notes')
+  const app = express()
+  app.use(jsonParser)
+
+  app.post('/api/notes', (req, res) => {
+    notes
+      .save(req.body, (err, result) => {
+        if (err) {
+          console.error(err)
+          res.sendStatus(500)
+        }
+        console.log(req.body)
+        res.sendStatus(201)
+      })
+  })
+
+  app.listen(3000, () => {
+    console.log('Visit http://localhost:3000')
+  })
+
+})
